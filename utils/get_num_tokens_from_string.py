b4e3448c17fe1724a4fa45e6a10ed34a4c058f2a
Rajesh Ghosh <rajesh.ghosh.here@gmail.com>
2024-01-01 10:50:23 +0530

    First

--- utils/get_num_tokens_from_string.py
+++
@@ -1,8 +0,0 @@
-import tiktoken
-def get_num_tokens_from_string(string: str, encoding_name: str) -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding("cl100k_base")
-    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
